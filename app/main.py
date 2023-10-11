782443027c9beadcfdbd33b2482de5ebc0a51dc8
hiroki <hiro@hirokinoMacBook-Pro.local>
2023-10-11 17:07:44 +0900

    delete db

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine, MetaData, Table, select
-
-DATABASE_URL = "mysql+mysqldb://[USERNAME]:[PASSWORD]@[HOST]:[PORT]/[DATABASE]"
-
-engine = create_engine(DATABASE_URL)
-metadata = MetaData()
-
-some_table = Table("some_table", metadata, autoload_with=engine)
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    with engine.connect() as connection:
-        result = connection.execute(select([some_table])).first()
-        return {"message": result["some_field"]}
+    return {"message": "Hello, World!"}
 
 @app.get("/version")
 def get_version():
