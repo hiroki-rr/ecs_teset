8493ede31e2dbc7558ca55c857023ee6d73807e7
hiroki <122215544+hiroki-rr@users.noreply.github.com>
2023-10-12 19:25:00 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,9 +4,9 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello, World!"}
+    return {"message": "Hello, World!!"}
 
 @app.get("/version")
 def get_version():
-    return {"version": "2.0", "message": "This is a new change for testing auto-deployment!"}
+    return {"version": "3.0", "message": "This is a new change for testing auto-deployment!"}
 
